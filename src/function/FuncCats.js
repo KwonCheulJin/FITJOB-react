aa6b2d4b33efcac66be05ad1f8deab95783161c5
KwonCheulJin <KwonCheulJin@users.noreply.github.com>
2021-07-31 14:33:15 +0900

    error state 추가

--- src/function/FuncCats.js
+++ src/function/FuncCats.js
@@ -9,6 +9,7 @@ import { useLocalStorage } from '../hooks/useLocalStorage'
 
 const FuncCats = () => {
 
+  const [error, setError] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
   // const [counter, setCounter] = useState(0)
   const [storedBreeds, storeBreeds] = useLocalStorage('breeds', [])
@@ -30,41 +31,42 @@ const FuncCats = () => {
     //useEffect에서 바로 async를 사용할 수 없다.
 
     const fetchData = async () => {
-
-      setIsLoading(true)
-
-      const breedsData = await getCatBreeds(currentPage, 10)
-
-      if (breedsData.length === 0) {
+      try {
+        setIsLoading(true)
+        const breedsData = await getCatBreeds(currentPage, 10)
+        if (breedsData.length === 0) {
+          setIsLoading(false)
+          return
+        }
+        // ----------- 4주차 내가 작성했던 코드 -----------
+        // setBreeds((previous) => {
+        //   // console.log(new Set(previous), previous.length, breedsData, currentPage)
+        //   function getUnique(breedsData, previous) {
+        //     return breedsData.map((value) => {
+        //       if (previous.length === undefined) return
+        //       for (let item of previous) {
+        //         if (value.id === item.id) return true
+        //       }
+        //     });
+        //   }
+        //   let result = getUnique(breedsData, previous)
+        //   console.log(result)
+        //   if (result[0]) return [...previous]
+        //   return [...previous, ...breedsData]
+        // })
+        // ----------- 4주차 내가 작성했던 코드 -----------
+        setBreeds((previous) => {
+          // console.log(previous)
+          const updatedBreeds = [...previous, ...breedsData]
+          storeBreeds(updatedBreeds)
+          return updatedBreeds
+        })
+      } catch (error) {
+        console.log(error)
+        setError(error)
+      } finally {
         setIsLoading(false)
-        return
       }
-
-      // setBreeds((previous) => {
-
-      //   // console.log(new Set(previous), previous.length, breedsData, currentPage)
-      //   function getUnique(breedsData, previous) {
-      //     return breedsData.map((value) => {
-      //       if (previous.length === undefined) return
-      //       for (let item of previous) {
-      //         if (value.id === item.id) return true
-      //       }
-      //     });
-      //   }
-
-      //   let result = getUnique(breedsData, previous)
-      //   console.log(result)
-      //   if (result[0]) return [...previous]
-      //   return [...previous, ...breedsData]
-      // })
-      setBreeds((previous) => {
-        // console.log(previous)
-        const updatedBreeds = [...previous, ...breedsData]
-        storeBreeds(updatedBreeds)
-        return updatedBreeds
-      })
-      setIsLoading(false)
-
     }
 
     if (storedPages.includes(currentPage)) {
@@ -85,12 +87,23 @@ const FuncCats = () => {
 
   return (
     <div className="Cats">
-      <CurrentPageNumber currentPage={currentPage} />
-      <HeaderButtonGroup onPreviousPage={handlePreviousPage} onNextPage={handleNextPage} />
-      <LoadingIndicator isLoading={isLoading} />
-      <BreedsList breeds={breeds} />
-      <LoadingIndicator isLoading={isLoading} />
-      <HeaderButtonGroup onPreviousPage={handlePreviousPage} onNextPage={handleNextPage} />
+      {error === null ? (
+        <>
+          <CurrentPageNumber currentPage={currentPage} />
+          <HeaderButtonGroup onPreviousPage={handlePreviousPage} onNextPage={handleNextPage} />
+          <LoadingIndicator isLoading={isLoading} />
+          <BreedsList breeds={breeds} />
+          <LoadingIndicator isLoading={isLoading} />
+          <HeaderButtonGroup onPreviousPage={handlePreviousPage} onNextPage={handleNextPage} />
+        </>
+      ) : (
+        <p>
+          데이터를 불러오는 도중 에러가 발생했습니다.
+          <br />
+          {JSON.stringify(error, null, 2)}
+        </p>
+      )}
+
     </div>
   )
 }
